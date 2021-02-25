75302ac6a44a25c8f03392670c82c1c37870c107
Arenzon <atlantidka1@gmail.com>
2021-02-26 00:30:06 +0300

    Сorrection LikesInfo.java

--- src/main/java/ru/netology/domain/LikesInfo.java
+++ src/main/java/ru/netology/domain/LikesInfo.java
@@ -1,40 +1,9 @@
 package ru.netology.domain;
 
 public class LikesInfo {
-    private int count;
-    private int userLikes;
-    private int canLike;
-    private int canPublish;
+    int count;
+    int userLikes;
+    boolean canLike;
+    boolean canPublish;
 
-    public int getCount () {
-        return count;
-    }
-
-    public void setCount ( int count ) {
-        this.count = count;
-    }
-
-    public int getUserLikes () {
-        return userLikes;
-    }
-
-    public void setUserLikes ( int userLikes ) {
-        this.userLikes = userLikes;
-    }
-
-    public int getCanLike () {
-        return canLike;
-    }
-
-    public void setCanLike ( int canLike ) {
-        this.canLike = canLike;
-    }
-
-    public int getCanPublish () {
-        return canPublish;
-    }
-
-    public void setCanPublish ( int canPublish ) {
-        this.canPublish = canPublish;
-    }
 }
